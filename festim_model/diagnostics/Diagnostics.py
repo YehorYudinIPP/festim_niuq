33841cf75f408e0d5d4b1a4b01c81f49d8a1e91d
Yehor Yudin <yehor.yudin@proton.me>
2025-07-25 16:02:36 +0100

    FESTIM model diagnostics: visualisation of multiple fields 	- added iteration over QoI 	- refactored code

--- festim_model/diagnostics/Diagnostics.py
+++ festim_model/diagnostics/Diagnostics.py
@@ -22,20 +22,29 @@ class Diagnostics:
         """
         self.model = model
         self.results = results
-        self.result_folder = result_folder if result_folder else './results'
-
-        # TODO by default, try to read results from the model attribute
+        self.result_folder = result_folder if result_folder else './results' # TODO by default, try to read results from the model attribute
 
         # If result is none, read from the result folder
         if self.results is None:
-            print(f"Diagniostics: No results provided, trying to read from {self.result_folder}/results.txt")
-            result_file = os.path.join(self.result_folder, 'results.txt')
-            if os.path.exists(result_file):
-                self.results = np.genfromtxt(result_file, skip_header=True, delimiter=',')
-                print(f"Results loaded from {result_file}")
-            else:
-                print(f"Warning: Results file not found: {result_file}")
-                self.results = None
+            print(f"Diagnostics: No results provided, trying to read from {self.result_folder}/")
+            self.results = {}
+
+            print(f">>> Diagnostics.model.quantities_of_interest: {self.model.quantities_of_interest}")  ###DEBUG print available quantities of interest
+
+            # Read results for each quantity of interest
+            for qoi,_ in self.model.quantities_of_interest.items():
+                # Read results from the file if available
+                print(f"Reading results for {qoi} from {self.result_folder}")
+
+                result_file = os.path.join(self.result_folder, f'results_{qoi}.txt')
+
+                # Check if the result file exists
+                if os.path.exists(result_file):
+                    self.results[qoi] = np.genfromtxt(result_file, skip_header=True, delimiter=',')
+                    print(f"Results for {qoi} loaded from {result_file}")
+        else:
+            print(f"Warning: No results for {qoi} file found in {self.result_folder}. Please run the simulation first.")
+            self.results[qoi] = None
 
         # Check if results are now present
         # TODO think if the flag is needed
@@ -93,7 +102,52 @@ class Diagnostics:
         )
 
         return derived_quantities
-    
+
+    def _visualise_transient_quantity(self, qoi_name, qoi_values):
+        """
+        Visualize a specific quantity of interest.
+        
+        :param qoi_name: Name of the quantity to visualize.
+        :param quantity: The quantity data to visualize.
+        """
+        if qoi_values is None:
+            print(f"No data available for {qoi_name}. Skipping visualization.")
+            return
+
+        plt.figure(figsize=(10, 6))
+
+        # iterate over the milestone times and plot each
+        for (i,time) in enumerate(self.milestone_times):
+            # The first column is the radial coordinate values
+            # TODO: read file a CSV
+            # TODO: consider that there might be no result data for a given milestone time
+            print(f"> Plotting results for time {time} [s]")
+
+            plt.plot(
+                self.model.vertices[:], 
+                qoi_values[:, i+1], 
+                label=f"t={time:.2f} s", 
+                #marker='o',
+                )
+
+            # n_el_print = 3
+            # print(f"Last {n_el_print} elements at time {time} s: {self.results[-n_el_print:, i+1]}") ### DEBUG
+
+        # Set plot labels and title
+        plt.xlabel('Radial Coordinate [m]')
+        plt.ylabel(f"{qoi_name} [m^-3]") 
+        # TODO: make printable names if quantities in a dictionary or attribute of quantities_of_interest
+        # TODO make a dictionary of units for each quantity, save this in an attribute for the quantity object/data structure
+        plt.title(f"{qoi_name} vs Radius (at different times) \n Param-s: T={self.model.config['model_parameters']['T_0']:.2f} [K], G={float(self.model.config['source_terms']['source_value']):.2e} [m^-3s^-1], C(a)={float(self.model.config['boundary_conditions']['right_bc_value']):.2e} [m^-3]")
+
+        plt.grid('both')
+        plt.legend(loc='best')
+        #plt.legend([f"t={time}" for time in self.milestone_times])
+
+        #plt.show()
+        plt.savefig(f"{self.result_folder}/results_{qoi_name}.png")
+        plt.close('all')  # Close all figures after plotting
+            
     def visualise(self):
         """
         Visualize the results of the FESTIM simulation.
@@ -120,30 +174,27 @@ class Diagnostics:
 
             if self.model.model.settings.transient:
                 print("Transient simulation detected, plotting results over time.")
-                for (i,time) in enumerate(self.milestone_times):
-                    # The first column is the radial coordinate values
-                    # TODO: read file a CSV
-                    # TODO: consider that there might be no result data for a given milestone time
-                    print(f"> Plotting results for time {time} s")
-                    plt.plot(self.model.vertices[:], self.results[:, i+1], label=f"t={time} s")
 
-                    # n_el_print = 3
-                    # print(f"Last {n_el_print} elements at time {time} s: {self.results[-n_el_print:, i+1]}") ### DEBUG
+                # Iterate over each quantity of interest in the results dictionary and plot
+                for qoi_name, qoi_values in self.results.items():
+                    print(f"Visualising quantity of interest: {qoi_name}")
+
+                    if qoi_values is not None:
+                        self._visualise_transient_quantity(qoi_name, qoi_values)
             else:
-                print("Steady-state simulation detected, only enty plotted.")
+                print("Steady-state simulation detected, only entry plotted.")
                 # Plot the single plot (t=steady)
-                plt.plot(self.model.vertices[:], self.results[:, -1], label=f"t=steady")
-                #plt.semilogy(self.model.vertices[:], self.results[:, -1], label=f"t=steady")
+                for qoi in self.model.quantities_of_interest:
+                    print(f"Visualising quantity of interest: {qoi_name}")
 
-            plt.xlabel("r [m]")
-            plt.ylabel("Concentration [m^-3]")
-            plt.title(f"Concentration vs Radius (at different times) \n Param-s: T={self.model.config['model_parameters']['T_0']:.2f} [K], G={float(self.model.config['source_terms']['source_value']):.2e} [m^-3s^-1], C(a)={float(self.model.config['boundary_conditions']['right_bc_value']):.2e} [m^-3]")
-            plt.grid('both')
-            plt.legend(loc='best')
-            #plt.legend([f"t={time}" for time in self.milestone_times])
-            #plt.show()
-
-            plt.savefig(f"{self.result_folder}/results.png")
+                    # Check if the results for this quantity are available
+                    if self.results[qoi] is not None:
+                        self._visualise_quantity(qoi, self.results[qoi]) #TODO implement for the steady state case
+                    else:
+                        print(f"No results available for {qoi}. Skipping visualization.")
+                    
+                        #plt.plot(self.model.vertices[:], self.results[:, -1], label=f"t=steady")
+                        #plt.semilogy(self.model.vertices[:], self.results[:, -1], label=f"t=steady")
 
         else:
             print("No results to visualize. Please run the simulation first.")
