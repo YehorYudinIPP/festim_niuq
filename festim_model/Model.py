af1e0c4baf8ec569a6c76d92237375a27591b0e7
Yehor Yudin <yehor.yudin@proton.me>
2025-08-08 15:08:34 +0100

    FESTIM Model: - fixed heat transfer problem init 		- fixed heat source init 		- refactoring 		- comments and debug outputs

--- festim_model/Model.py
+++ festim_model/Model.py
@@ -81,10 +81,10 @@ class Model():
         # print (f" >> Using heat transfer model: {self.model.T.__dict__}") ###DEBUG
 
         # Define Boundary conditions
-        self._specify_boundary_conditions(config)
+        # self._specify_boundary_conditions(config)
 
         # Define source terms
-        self._add_source_terms(config)
+        # self._add_source_terms(config)
 
         # Define derived quantities - TODO implement
         #self.derived_quantities = self.define_derived_quantities()
@@ -107,7 +107,7 @@ class Model():
 
         print(f" > Initialisation finished! Model initialized with {self.n_elements} elements")  ###DEBUG
 
-    def _specify_geometry(self, config):
+    def _specify_geometry(self, config, refine_flag=False):
         """
         Specify the geometry of the FESTIM model.
         This method can be extended to include specific geometry logic.
@@ -130,24 +130,26 @@ class Model():
         self.vertices = np.linspace(0., self.length, self.n_elements + 1)
 
         # When the BC uncertainty influence fall-off quickly, try refined mesh at the domain boundary
+
         # Option 2) mesh refined at the boundary (right side)
-        refined_length_fraction = 0.1  # Fraction of the length to refine
-        refined_elements_fraction = 0.25  # Fraction of elements to refine
+        if refine_flag: 
+            refined_length_fraction = 0.1  # Fraction of the length to refine
+            refined_elements_fraction = 0.25  # Fraction of elements to refine
+            refined_elements_count = int(self.n_elements * refined_elements_fraction)
 
-        refined_elements_count = int(self.n_elements * refined_elements_fraction)
-        # self.vertices = np.concatenate((
-        #     np.linspace(0., self.length * (1. - refined_length_fraction), self.n_elements - refined_elements_count + 1), # inner (larger) part of the mesh
-        #     np.linspace(self.length * (1. - refined_length_fraction), self.length, refined_elements_count + 1)[1:] # refined (smaller outer) part of the mesh
-        # ))
-        #TODO mind round-off errors in the mesh size
+            self.vertices = np.concatenate((
+                np.linspace(0., self.length * (1. - refined_length_fraction), self.n_elements - refined_elements_count + 1), # inner (larger) part of the mesh
+                np.linspace(self.length * (1. - refined_length_fraction), self.length, refined_elements_count + 1)[1:] # refined (smaller outer) part of the mesh
+            ))
+            #TODO mind round-off errors in the mesh size
 
-        #print(f"Using vertices: {self.vertices}")  ### Debugging output
+        #print(f"Using vertices: {self.vertices}")  ###DEBUG output
 
         # Create a Mesh object from the vertices
 
         # Option 1) for mesh: use FESTIM's MeshFromVertices
         self.model.mesh = F.MeshFromVertices(
-            type=self.coordinate_system_type,  # Specify (spherical) mesh type; available coordinate systems: 'cartesian', 'cylindrical', 'spherical'; default is Cartesian
+            #type=self.coordinate_system_type,  # Specify (spherical) mesh type; available coordinate systems: 'cartesian', 'cylindrical', 'spherical'; default is Cartesian
             vertices=self.vertices,  # Use the vertices defined above
             )
         #TODO: add a fallback for unsupported coordinate systems
@@ -157,7 +159,7 @@ class Model():
         #     type="spherical",  # Specify spherical mesh type
         # )
 
-        print(f" >> Using mesh object: {self.model.mesh.__dict__}")  ###DEBUG
+        #print(f" >> Using mesh object: {self.model.mesh.__dict__}")  ###DEBUG
         return self.model.mesh
 
     def _specify_boundary_conditions(self, config, quantity='concentration'):
@@ -249,16 +251,20 @@ class Model():
         print(f" > config['materials'] = \n {config['materials']}")  ###DEBUG
 
         # Define material properties
-        self.model.materials = F.Material(
+        material = F.Material(
             id=1,
+            
             D_0=float(config['materials']['D_0']),  # diffusion coefficient
             E_D=float(config['materials']['E_D']),  # activation energy
             thermal_cond=float(config['materials']['thermal_conductivity']),  # thermal conductivity
             rho=float(config['materials']['rho']),  # density
             heat_capacity=float(config['materials']['heat_capacity']),  # specific heat capacity
+            
             #solubility=float(config['materials']['solubility']),  # solubility
             )
         # TODO: fetch data from HTM DataBase - LiO2 as an example (absent in HTM DB)
+
+        self.model.materials = material
 
         #print(f"Using material properties: D_0={self.model.materials[0].D_0}, E_D={self.model.materials[0].E_D}, T={self.model.T.__dict__}") ###DEBUG
 
@@ -319,16 +325,20 @@ class Model():
 
         # Add model for T, temperature quantity, and redefine the model's attribute
         if self.model.settings.transient:
+
             self.model.T = F.HeatTransferProblem(
                 transient=True,
                 initial_condition=F.InitialCondition(
                     value=float(config['initial_conditions']['temperature']['value']),  # Initial temperature [K]
                     field="T",
                 ),
-                absolute_tolerance=float(config['simulation']['absolute_tolerance']),  #  absolute tolerance
-                relative_tolerance=float(config['simulation']['relative_tolerance']),  #  relative tolerance     
+                # absolute_tolerance=float(config['simulation']['absolute_tolerance']),  #  absolute tolerance
+                # relative_tolerance=float(config['simulation']['relative_tolerance']),  #  relative tolerance     
             )
-            print(f" >> Using transient heat problem with the initial temperature: {self.model.T.initial_condition.value} [K]")  # Debugging output
+
+            #self.model.T = F.HeatTransferProblem(transient=True, initial_condition=F.InitialCondition(field="T", value=300)) ###DEBUG
+
+            print(f" >> Using transient heat problem with the initial temperature: {self.model.T.initial_condition.value} [K]")  ###DEBUG output
         else:
             self.model.T = F.HeatTransferProblem(
                 transient=False,
@@ -340,13 +350,13 @@ class Model():
         h_coeff = float(config['boundary_conditions']['temperature']['right']['hcoeff_value'])  # Convective heat transfer coefficient [W/(m²*K)] #TODO make a fallback or error if not specified
 
         # If exists, apply a heat source term
-        self.model.T.source_terms = []  # Initialize source terms for heat transfer
+        self.model.sources = []  # Initialize source terms for heat transfer
 
         if 'heat' in config['source_terms']:
             if config['source_terms']['heat']['source_type'] == 'constant':
                 Q_source = float(config['source_terms']['heat']['source_value'])  # Heat source term [W/m³]
 
-                self.model.T.source_terms.append(
+                self.model.sources.append(
                     F.Source(
                         value=Q_source,  # Source term value
                         volume=1,  # Assuming a single volume for the entire mesh
@@ -354,11 +364,13 @@ class Model():
                     )
                 )
 
+                #self.model.sources = [F.Source(value=100, field="T", volume=1)] ###DEBUG
+
         else:
             print(f"Warning: No heat source term specified, using Q_source = 0.0 W/m³")
             Q_source = 0.0
 
-        print(f" >> Using source term for heat transfer: Q_source={Q_source} [W/m³]")  # Debugging output
+        print(f" >> Using source term for heat transfer: Q_source={Q_source} [W/m³]")  ###DEBUG output
 
         # Apply appropriate boundary conditions for heat transfer
         surfaces_nums = [1, 2]
@@ -372,8 +384,8 @@ class Model():
                 'left': {'type': 'dirichlet', 'value': 300.0},  # Default left boundary temperature [K]
                 'right': {'type': 'neumann', 'value': 0.0}  # Default right boundary temperature gradient [K]
             }
-        
-        # iterate over surfaces and apply boundary conditions
+
+        # Iterate over surfaces and apply boundary conditions
         for surface_name, surface_num in surface_map.items():
             print(f" >>> Applying HEAT boundary conditions for surface {surface_name} (surface number {surface_num})")  ###DEBUG
 
@@ -388,7 +400,7 @@ class Model():
                 # - 1) Dirichlet BC: fixed temperature
                 if bc['type'] == 'dirichlet':
                     # Dirichlet boundary condition
-                    self.model.T.boundary_conditions.append(
+                    self.model.boundary_conditions.append(
                         F.DirichletBC(
                             value=float(bc['value']),  # Boundary condition value
                             surfaces=[surface_num],  # Apply to the specified surface
@@ -400,35 +412,37 @@ class Model():
                 # - 2) Neumann BC: fixed flux
                 elif bc['type'] == 'neumann':
                     # Neumann boundary condition (Flux)
-                    self.model.T.boundary_conditions.append(
+                    self.model.boundary_conditions.append(
                         F.FluxBC(
                             value=float(bc['value']),  # Boundary condition value
                             surfaces=[surface_num],  # Apply to the specified surface
                             field="T",  # Field for the boundary condition
                         )
                     )
-                    print(f" >>> Using Neumann BC at surface {surface_num} with value {bc['value']} [K m^-1]")  # Debugging output
+                    print(f" >>> Using Neumann BC at surface {surface_num} with value {bc['value']} [K m^-1]")  ###DEBUG output
 
                 # - 3) Convective flux BC: convective heat transfer
                 elif bc['type'] == 'convective_flux':
                     # Convective flux boundary condition
-                    self.model.T.boundary_conditions.append(
+                    self.model.boundary_conditions.append(
                         F.ConvectiveFlux(
                             h_coeff=h_coeff,  # Convective heat transfer coefficient [W/(m²*K)]
                             T_ext=float(bc['value']),  # External temperature [K]
                             surfaces=[surface_num],  # Apply to the specified surface
-                            field="T",  # Field for the boundary condition
+                            #field="T",  # Field for the boundary condition
                         )
                     )
-                    print(f" >?> Using Convective Flux BC at surface {surface_num} with h_coeff {h_coeff} [W/(m²*K)] and T_ext {bc['value']} [K]")  # Debugging output
+                    print(f" >>> Using Convective Flux BC at surface {surface_num} with h_coeff {h_coeff} [W/(m²*K)] and T_ext {bc['value']} [K]")  ###DEBUG output
                 else:
                     raise ValueError(f"Unknown or unsupported boundary condition type: {bc['type']}")
             else:
                 print(f"Warning: No temperature boundary condition specified for surface {surface_name}, using default values.")
 
+        #self.model.boundary_conditions = [F.DirichletBC(surfaces=[1, 2], value=400, field="T")] ###DEBUG
+
         #print(f" >> Using boundary conditions for temperature: T={self.model.T.boundary_conditions[0].value} [K] at surface 1, h_coeff={h_coeff}, T_ext={config['boundary_conditions']['temperature']['right']['value']} [K] at surface 2")  # Debugging output
         
-        print(f" >> Using heat transfer model: {self.model.T.__dict__}")  ###DEBUG output
+        print(f" >> Using heat transfer model: {self.model.T.__dict__}")  ###DEBUG
         return self.model.T
 
     def _specify_time_integration_settings(self, config):
@@ -439,14 +453,16 @@ class Model():
         print("Specifying time integration settings...")
 
         # Define time integration settings for the simulation
-        self.model.dt = F.Stepsize(
-            #float(config['simulation']['time_step']), # op1) fixed time step size
+        dt = float(config['simulation']['time_step'])
 
-            initial_value=float(config['simulation']['time_step']),  # op2) initial time step size for adaptive dt
-            stepsize_change_ratio=float(config['simulation']['stepsize_change_ratio']),  # ratio for adaptive time stepping
-            #min_value=float(config['simulation']['min_time_step']),  # minimum time step size
-            max_stepsize=float(config['simulation']['max_stepsize']),  # maximum time step size
-            dt_min=1e-05,  # minimum time step size for adaptive time stepping
+        self.model.dt = F.Stepsize(
+            dt, # op1) fixed time step size
+
+            # initial_value=dt,  # op2) initial time step size for adaptive dt
+            # stepsize_change_ratio=float(config['simulation']['stepsize_change_ratio']),  # ratio for adaptive time stepping
+            # #min_value=float(config['simulation']['min_time_step']),  # minimum time step size
+            # max_stepsize=float(config['simulation']['max_stepsize']),  # maximum time step size
+            # dt_min=1e-05,  # minimum time step size for adaptive time stepping
 
             milestones=self.milestone_times, # check points for results export
         ) 
